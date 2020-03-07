27bd9bcd4bb3fa30c85b6ffd15a98a903d96567d
mika <mika.morsky@gmail.com>
2020-03-07 14:39:51 +0200

    Send to mqtt broker added

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
 const axios = require('axios');
+const mqtt = require('mqtt');
 
 
-
-function LueRobo(delta){
-axios.get('https://fanuc-robot-http-server.herokuapp.com/')
-.then((res) => {
-    //console.log(res);
-    const regexp = /Joint.*\:\s*([-|\d].*)/gi;
-    let joints = [];
-    let matches = res.data.matchAll(regexp);
-    let count = 0;
-    for (const match of matches) {
-        count++;
-        
-        if (count > 6) break;
-            const value = parseFloat(match[1]);
-            joints.push(value);
-
-    }
-
-    console.log(Date() + "  " + joints + "  " + delta + " ms");
+const mqtt_client = mqtt.connect('wss://mqtt-worker.herokuapp.com');
+mqtt_client.on('connect', () => {
+    console.log('connected to mqtt broker');
+    main_loop();
 });
-    return
-}
-start_time_stamp = new Date();
+
 const main_loop = () => {
-    
     setTimeout(() => {
-        //Koodi
-        
+
+    let start_time_stamp = new Date();
+    axios.get('https://fanuc-robot-http-server.herokuapp.com/')
+    .then((res) => {
         const time_stamp = new Date();
         const delta = time_stamp - start_time_stamp;
-        LueRobo(delta);
+        const regexp = /Joint.*\:\s*([-|\d].*)/gi;
+        let joints = [];
+        let matches = res.data.matchAll(regexp);
+        let count = 0;
+        for (const match of matches) {
+            count++;
+            if (count > 6) break;
+                const value = parseFloat(match[1]);
+                joints.push(value);
+        }
+
+        console.log(Date() + "  " + joints + "  " + delta + " ms");
+        let data = {
+            time: time_stamp,
+            joints: joints
+            };
+        mqtt_client.publish('joints', JSON.stringify(data));
         main_loop();
-    }, 1000);
+    });
+},100);
 }
-main_loop();
